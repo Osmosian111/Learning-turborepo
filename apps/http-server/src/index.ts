aa38fcf25eb8a4ca6376db3c5fd632b466e43498
Osmosiac111 <neevjain482@gmail.com>
2025-05-31 00:57:10 +0530

    Upgrading repo

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express()
 
+const PORT = 3001
+
 app.use(express.json())
 
 app.post("signin",(req,res)=>{res.send({msg:"signin"})});
 app.post("signup",(req,res)=>{res.send({msg:"signup"})});
 app.get("chat",(req,res)=>{res.send({msg:"signup"})});
+
+app.listen(PORT)
